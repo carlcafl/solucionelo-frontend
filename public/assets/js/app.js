4b9db607618d5ee2938ba72cdf6a99de66905b02
carlcafl <ccarmona@gmail.com>
2016-07-06 17:59:05 -0500

    Formulario mejorado

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -3,6 +3,34 @@ var backendURL = "https://solucionelo-backend.herokuapp.com/api/rest";
 var app = angular.module('solucioneloApp', []);		
 
 app.controller('RegistrationCtrl',function($scope, $http) {
+	$scope.idTypes = [
+	                  {
+	                	  id: "CC",
+	                	  name: "Cedula de ciudadania"
+	                  },
+	                  {
+	                	  id: "PA",
+	                	  name: "Pasaporte"
+	                  },
+	                  {
+	                	  id: "TI",
+	                	  name: "Tarjeta de identidad"
+	                  }
+	                  ]
+	$scope.user = {
+			idType: null,
+			idNumber: null,
+			registeredDate: null,
+			ipAddress: null,
+			firstName: null,
+			lastName: null,
+			phoneNumber: null,
+			mobileNumber: null,
+			email: null,
+			city: null,
+			offeredServices: []
+	};
+	$scope.services = null;
 	$http.get(backendURL + '/services'
 	).success(function(data) {			
 		$scope.services = data;
