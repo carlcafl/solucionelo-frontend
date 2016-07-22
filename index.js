033b6402b5e2db037174f8c19f950a1c8d192485
carlcafl <ccarmona@gmail.com>
2016-07-22 15:17:52 -0500

    Limpieza HTML + SEO + Security Header

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+app.use(function(req, res, next) {
+	//Header de proteccion XSS
+    res.setHeader("X-XSS-Protection", "1; mode=block");
+    //Header de proteccion de politica de seguridad
+    //res.setHeader("Content-Security-Policy", "default-src 'unsafe-inline' *; script-src 'unsafe-inline' *; connect-src 'unsafe-inline' *; img-src *; style-src 'unsafe-inline' *;");
+    return next();
+  });
+app.disable('x-powered-by');
 app.use(express.static(__dirname + '/public'));
 
 app.listen(app.get('port'), function() {
